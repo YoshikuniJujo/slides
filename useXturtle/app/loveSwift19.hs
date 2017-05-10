3bf0e93cc6ec40317adfe1efba8f80d26cf313a0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-05-10 10:41:17 +0900

    Show multiple pages

--- useXturtle/app/loveSwift19.hs
+++ useXturtle/app/loveSwift19.hs
@@ -9,9 +9,23 @@ version = [0, 1, 0, 0]
 
 main :: IO ()
 main = do
-	runLecture version (titlePage :| [])
+	runLecture version $ titlePage :| [
+		samplePage,
+		samplePage2
+		]
 
 titlePage :: Page
 titlePage = [
 	writeTitle "Swiftの中心で、Haskellをさけぶ" "第19回 Swift愛好会"
+	]
+
+samplePage, samplePage2 :: Page
+samplePage = [
+	text "サンプルのページですよ",
+	text "これは2行目です"
+	]
+
+samplePage2 = [
+	text "サンプルのページの2つめです",
+	text "これは2つめの2行目です"
 	]
--- useXturtle/src/Lecture.hs
+++ useXturtle/src/Lecture.hs
@@ -1,12 +1,14 @@
 module Lecture (
-	Page, Version, runLecture, writeTitle
+	Page, Version, runLecture, writeTitle, text
 	) where
 
 import Control.Applicative
+import Control.Concurrent
 import Data.Maybe
 import Data.List
-import Data.List.NonEmpty (NonEmpty(..))
+import Data.List.NonEmpty (NonEmpty(..), (<|))
 import Data.Function
+import Data.IORef
 import System.Exit
 
 import Graphics.X11.Turtle
@@ -24,21 +26,30 @@ data Setting = Setting {
 	stRatio :: Maybe Double,
 	stPageTurtle :: Maybe Turtle,
 	stBodyTurtle :: Maybe Turtle,
-	stAllPages :: Maybe Int }
+	stAllPages :: Maybe Int,
+	stPageNumber :: Maybe (IORef Int),
+	stPageZipper :: Maybe (IORef (Zipper Page)),
+	stPageEnd :: Maybe (IORef Bool) }
 
 initialSetting :: Setting
 initialSetting = Setting {
 	stRatio = Nothing,
 	stPageTurtle = Nothing,
 	stBodyTurtle = Nothing,
-	stAllPages = Nothing }
+	stAllPages = Nothing,
+	stPageNumber = Nothing,
+	stPageZipper = Nothing,
+	stPageEnd = Nothing }
 
 appendSettings :: Setting -> Setting -> Setting
 appendSettings s1 s2 = Setting {
 	stRatio = stRatio s1 <|> stRatio s2,
 	stPageTurtle = stPageTurtle s1 <|> stPageTurtle s2,
 	stBodyTurtle = stBodyTurtle s1 <|> stBodyTurtle s2,
-	stAllPages = stAllPages s1 <|> stAllPages s2 }
+	stAllPages = stAllPages s1 <|> stAllPages s2,
+	stPageNumber = stPageNumber s1 <|> stPageNumber s2,
+	stPageZipper = stPageZipper s1 <|> stPageZipper s2,
+	stPageEnd = stPageEnd s1 <|> stPageEnd s2 }
 
 setTurtles :: Turtle -> Turtle -> Setting -> Setting
 setTurtles p t s = s {
@@ -47,6 +58,15 @@ setTurtles p t s = s {
 
 setAllPages :: Int -> Setting -> Setting
 setAllPages p s = s { stAllPages = Just p }
+
+setPageNumber :: IORef Int -> Setting -> Setting
+setPageNumber pn s = s { stPageNumber = Just pn }
+
+setPageZipper :: IORef (Zipper Page) -> Setting -> Setting
+setPageZipper pz s = s { stPageZipper = Just pz }
+
+setPageEnd :: IORef Bool -> Setting -> Setting
+setPageEnd pe s = s { stPageEnd = Just pe }
 
 instance Monoid Setting where
 	mempty = initialSetting
@@ -59,7 +79,10 @@ data State = State {
 	ratio :: Double,
 	pageTurtle :: Turtle,
 	bodyTurtle :: Turtle,
-	allPages :: Int }
+	allPages :: Int,
+	pageNumber :: IORef Int,
+	pageZipper :: IORef (Zipper Page),
+	pageEnd :: IORef Bool }
 
 settingToState :: Setting -> IO State
 settingToState st = fromMaybe
@@ -68,7 +91,10 @@ settingToState st = fromMaybe
 		p <- stPageTurtle st
 		t <- stBodyTurtle st
 		ap <- stAllPages st
-		return (return $ State r p t ap)
+		pn <- stPageNumber st
+		pz <- stPageZipper st
+		pe <- stPageEnd st
+		return (return $ State r p t ap pn pz pe)
 
 width, height :: State -> Double
 width = (512 *) . ratio
@@ -77,15 +103,39 @@ height = (375 *) . ratio
 fontName :: String
 fontName = "KochiGothic"
 
-data Zipper a = Zipper [a] [a] deriving Show
+data Zipper a = Zipper [a] (NonEmpty a) deriving Show
+
+nonEmptyToZipper :: NonEmpty a -> Zipper a
+nonEmptyToZipper = Zipper []
+
+peekZipper :: Zipper a -> a
+peekZipper (Zipper _ (x :| _)) = x
+
+nextZipper, backZipper :: Zipper a -> Maybe (Zipper a)
+nextZipper (Zipper b n) = case NE.uncons n of
+	(x, Just n') -> Just $ Zipper (x : b) n'
+	(_, Nothing) -> Nothing
+
+backZipper (Zipper (x : b) n) = Just $ Zipper b (x <| n)
+backZipper (Zipper [] _) = Nothing
+
+getPage :: State -> IO Page
+getPage = (peekZipper <$>) . readIORef . pageZipper
+
+nextPage :: State -> IO ()
+nextPage st = do
+	z <- readIORef $ pageZipper st
+	case nextZipper z of
+		Just z' -> do
+			writeIORef (pageZipper st) z'
+			modifyIORef (pageNumber st) (+ 1)
+		Nothing -> return ()
 
 runLecture :: Version -> NonEmpty Page -> IO ()
-runLecture v pga@(pg :| pgs) = do
+runLecture v pga = do
 	os <- getOptions
 	st <- (mergeSettings <$>) $ sequence . map snd
 		. sortBy (compare `on` fst) . map (getAction v pga) $ fst os
-
-	let	rt = getRatio st
 	fld <- openField
 	topleft fld
 	p <- newTurtle fld
@@ -94,24 +144,37 @@ runLecture v pga@(pg :| pgs) = do
 	t <- newTurtle fld
 	hideturtle t
 	penup t
-	stt <- settingToState . setAllPages (NE.length pga) $ setTurtles p t st
-	runPage stt pg
+	pn <- newIORef 1
+	pz <- newIORef $ nonEmptyToZipper pga
+	pe <- newIORef True
+	stt <- settingToState
+		. setPageEnd pe
+		. setPageZipper pz
+		. setPageNumber pn
+		. setAllPages (NE.length pga)
+		$ setTurtles p t st
+	_ <- forkIO $ runPage stt
 	onkeypress fld $ \c -> do
 		case c of
 			'q' -> return False
+			' ' -> forkIO (runPage stt) >> return True
 			_ -> return True
 
 	waitField fld
 
-runPage :: State -> Page -> IO ()
-runPage st pg = do
+runPage :: State -> IO ()
+runPage st = do
+	clear p >> clear t
 	goto p (width st * 44 / 50) (height st * 48 / 50)
-	write p fontName (12 * rt) $ show (1 :: Int)
+	write p fontName (12 * rt) . show =<< readIORef (pageNumber st)
 	forward p (24 * rt)
 	write p fontName (12 * rt) $ "/" ++ show ap
-	goto t 0 0
+	goto t (width st / 8) (height st / 8)
 	showturtle t
-	sequence_ $ map ($ st) pg
+	sequence_
+		. intersperse (setheading t (- 90) >> forward t (24 * rt))
+		. map ($ st) =<< getPage st
+	nextPage st
 	sleep t 500
 	hideturtle t
 	where
@@ -146,5 +209,15 @@ writeTitle ttl sttl st = do
 		(height st * 1 / 2 + 20 * ratio st)
 	write t fontName (12 * ratio st) sttl
 	speed t "slow"
+	where
+	t = bodyTurtle st
+
+text :: String -> State -> IO ()
+text tx st = do
+	setx  t $ width st / 8
+	setheading t 0
+	write t fontName (10 * ratio st) tx
+	showturtle t
+	hideturtle t
 	where
 	t = bodyTurtle st
