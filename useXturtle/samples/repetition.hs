64da534d0aa2073c792b1471aed75a95a2ec2213
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-06-10 07:36:17 +0900

    Haskell ハンズオン: メモ化した、お金のはらいかた

--- useXturtle/samples/repetition.hs
+++ useXturtle/samples/repetition.hs
@@ -16,3 +16,23 @@ cases _ am | am < 0 = 0
 cases _ 0 = 1
 cases [] _ = 0
 cases ca@(c : cs) am = cases ca (am - c) + cases cs am
+
+type Memo = [(([Integer], Integer), Integer)]
+type Memoized = Memo -> (Integer, Memo)
+
+ret :: Integer -> Memoized
+ret x = \m -> (x, m)
+
+memo :: (([Integer], Integer) -> Memoized) -> ([Integer], Integer) -> Memoized
+memo f n s = case lookup n s of
+	Just v -> (v, s)
+	Nothing -> let (v, s1) = f n s in (v, (n, v) : s1)
+
+mcases :: ([Integer], Integer) -> Memoized
+mcases (_, am) | am < 0 = ret 0
+mcases (_, 0) = ret 1
+mcases ([], _) = ret 0
+mcases (ca@(c : cs), a) = \s -> let
+	(n1, s1) = memo mcases (ca, a - c) s
+	(n2, s2) = memo mcases (cs, a) s1 in
+	ret (n1 + n2) s2
