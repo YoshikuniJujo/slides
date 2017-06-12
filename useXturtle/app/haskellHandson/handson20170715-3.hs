493e0a4d41e7666a60fd0768304eed8c79a93ef8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-06-12 14:07:53 +0900

    Haskell ハンズオン: 3. IOモナド: スタブ

---
+++ useXturtle/app/haskellHandson/handson20170715-3.hs
@@ -0,0 +1,13 @@
+import Data.List.NonEmpty
+
+import Lecture
+
+version :: Version
+version = [0, 1, 0, 0]
+
+main :: IO ()
+main = runLecture version $ titlePage :| [
+	]
+
+titlePage :: Page
+titlePage = writeTitle "Haskell入門ハンズオン!" "3. IOモナド" :| []
