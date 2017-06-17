58fc18e36c17bf114a4e41119f96554af0ef9bdb
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-06-17 11:16:40 +0900

    スケジュールなど

---
+++ useXturtle/samples/calculator.hs
@@ -0,0 +1,22 @@
+type Parse a = String -> [(a, String)]
+
+succeed :: a -> Parse a
+succeed v inp = [(v, inp)]
+
+check :: (Char -> Bool) -> Parse Char
+check p (c : cs) | p c = [(c, cs)]
+check _ _ = []
+
+char :: Char -> Parse Char
+char c0 = check $ \c1 -> c1 == c0
+
+alt :: Parse a -> Parse a -> Parse a
+(p1 `alt` p2) inp = p1 inp ++ p2 inp
+
+build :: Parse a -> (a -> b) -> Parse b
+build p f inp = map (\(x, r) -> (f x, r)) $ p inp
+
+{-
+(>@>) :: Parse a -> Parse b -> Parse (a, b)
+(p1 >@> p2) inp = map (\
+-}
