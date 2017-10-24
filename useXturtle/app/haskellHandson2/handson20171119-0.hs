4cd004ebeec7bb6e6ef09a4cf3040dda8ec9b0dc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-10-24 15:54:30 +0900

    Haskell入門ハンズオン #2: 近況の雛形を追加

--- useXturtle/app/haskellHandson2/handson20171119-0.hs
+++ useXturtle/app/haskellHandson2/handson20171119-0.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Data.List.NonEmpty
 
 import Lecture
@@ -7,7 +8,36 @@ version = [0, 1, 0, 0]
 
 main :: IO ()
 main = runLecture version $ titlePage :| [
+	greeting, selfIntroduction, kinkyou
 	]
 
 titlePage :: Page
 titlePage = writeTitle "Haskell入門ハンズオン! #2" "0. はじめに" :| []
+
+greeting :: Page
+greeting = pageTitle "ごあいさつ" :| [
+	text "おあつまりいただき、ありがとうございます",
+	(>>)
+		<$> writeImageCenter
+			(515 * 22 / 60, 654 * 22 / 60, "images/cover.png")
+		<*> replicateM_ 10 . nextLine,
+	text "「Haskell 教養としての関数型プログラミング」の著者",
+	itext 4 "です"
+	]
+
+selfIntroduction :: Page
+selfIntroduction = pageTitle "自己紹介" :| [
+	]
+
+kinkyou :: Page
+kinkyou = pageTitle "近況" :| [
+	text "最近、水出しコーヒーの器具を買った",
+	(>>)
+		<$> writeImageCenter
+--			(120, 213, "images/naganeko.png")
+			(120, 195, "images/naganeko.png")
+		<*> replicateM_ 8 . nextLine,
+	text "(あとでさしかえる、画像の大きさなども調整する)",
+	text "1滴ずつ水をたらしながら何時間もかけて抽出する",
+	text "豆の挽きかた(細かくか、荒くか)など、いろいろ試している"
+	]
