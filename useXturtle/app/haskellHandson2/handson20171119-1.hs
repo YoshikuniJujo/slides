b94d5b3c86b55e04a69b73230fe564203a40a1a5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-10-30 07:40:53 +0900

    Haskell入門ハンズオン #2: 雛形: 「対話環境」「関数」

---
+++ useXturtle/app/haskellHandson2/handson20171119-1.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Data.List.NonEmpty
+
+import Lecture
+
+version :: Version
+version = [0, 1, 0, 0]
+
+main :: IO ()
+main = runLecture version $ titlePage :| [
+	]
+
+titlePage :: Page
+titlePage = writeTitle "Haskell入門ハンズオン! #2" "1. 対話環境を使う" :| []
