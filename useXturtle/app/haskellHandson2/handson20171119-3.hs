98619514dfca050eca8d78747ffbe42b4a13b3f5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-11-08 14:21:26 +0900

    Haskell入門ハンズオン #2: タプル、リストに対するパターンマッチ

--- useXturtle/app/haskellHandson2/handson20171119-3.hs
+++ useXturtle/app/haskellHandson2/handson20171119-3.hs
@@ -8,11 +8,39 @@ version = [0, 1, 0, 0]
 
 main :: IO ()
 main = runLecture version $ titlePage :| [
-	list1, list2
+	tuple1, tuple2,
+	list1, list2, list3
 	]
 
 titlePage :: Page
-titlePage = writeTitle "Haskell入門ハンズオン! #2" "3. リストとcase式" :| []
+titlePage = writeTitle
+	"Haskell入門ハンズオン! #2" "3. タプル、リスト、case式" :| []
+
+tuple1 :: Page
+tuple1 = pageTitle "タプル" :| [
+	text "複数の値をセットにして、ひとつの値にしたい?",
+	text "タプルを使っては、いかがでしょうか",
+	itext 4 "> (\"Yoshikuni Jujo\", 37)",
+	itext 4 "(\"Yoshikuni Jujo\",37)",
+	itext 4 "> yj = it",
+	text "変数itで直前に評価された値を取り出せる",
+	text "タプルのなかみを取り出すにもパターンマッチを使う",
+	itext 4 "> :{",
+	itext 4 "| showPerson (n, a) =",
+	itext 4 "|     n ++ \" (\" ++ show a ++ \")\"",
+	itext 4 "> :}",
+	itext 4 "> showPerson yj",
+	itext 4 "\"Yoshikuni Jujo (37)\""
+	]
+
+tuple2 :: Page
+tuple2 = pageTitle "タプル" :| [
+	text "タプルを使うと複数の値を、まとめてひとつの値にできる",
+	text "丸括弧でかこんで、値どうしをカンマでくぎる",
+	itext 4 "(\"foo\", 123)",
+	text "なかみを取り出すにはパターンマッチを使う",
+	itext 4 "bar (x, y) = ..."
+	]
 
 list1 :: Page
 list1 = pageTitle "リスト" :| [
@@ -44,4 +72,19 @@ list2 = pageTitle "リスト" :| [
 	text "x : xsのようなパターンでマッチしてやると",
 	itext 4 "先頭(3)が変数xを",
 	itext 4 "残り(8 : (5 : ...))が変数xsを束縛する"
+	]
+
+list3 :: Page
+list3 = pageTitle "リスト" :| [
+	text "先頭の値と残りのリストからなるタプルを作る関数",
+	text "空リストのときは値をもたない",
+	itext 4 "headTail [] = Nothing",
+	itext 4 "headTail (x : xs) = Just (x, xs)",
+	text "対話環境で試してみよう",
+	itext 4 "> :{",
+	itext 4 "| headTail [] = Nothing",
+	itext 4 "| headTail (x : xs) = Just (x, xs)",
+	itext 4 "> :}",
+	itext 4 "> headTail [3, 2, 8, 5, 4]",
+	itext 4 "Just (3,[2,8,5,4])"
 	]
