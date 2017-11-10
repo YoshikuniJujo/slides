107cedd9790c0ba7daa99c007ad06181088532df
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-11-10 15:38:28 +0900

    Haskell入門ハンズオン #2: スタブ: 5. いくつかの関数を学ぶ(1)

---
+++ useXturtle/app/haskellHandson2/handson20171119-4.hs
@@ -0,0 +1,15 @@
+import Control.Monad
+import Data.List.NonEmpty
+
+import Lecture
+
+version :: Version
+version = [0, 1, 0, 0]
+
+main :: IO ()
+main = runLecture version $ titlePage :| [
+	]
+
+titlePage :: Page
+titlePage = writeTitle
+	"Haskell入門ハンズオン! #2" "4. いくつかの関数を学ぶ(1)" :| []
