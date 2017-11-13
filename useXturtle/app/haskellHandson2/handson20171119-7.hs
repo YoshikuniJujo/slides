be767e127b42502b51b1ea83495b8a1918ffb45f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-11-13 17:44:52 +0900

    Haskell入門ハンズオン #2: スタブ: 「7. 入出力を学ぶ」

---
+++ useXturtle/app/haskellHandson2/handson20171119-7.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Data.List.NonEmpty
+
+import Lecture
+
+version :: Version
+version = [0, 1, 0, 0]
+
+main :: IO ()
+main = runLecture version $ titlePage :| [
+	]
+
+titlePage :: Page
+titlePage = writeTitle "Haskell入門ハンズオン! #2" "7. 入出力を学ぶ" :| []
